90f551cd9958d7224f028970d3fde5c14a49b14a
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-10-17 01:06:18 +0530

    feat: implemented helmet, xss, and mongo sanitizer

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,28 @@
 import "./types";
+import mongoSanitize from "express-mongo-sanitize";
 import express, { Response } from "express";
 import CookieParser from "cookie-parser";
+import helmet from "helmet";
+import { xss } from "express-xss-sanitizer";
 import cors from "cors";
 
-import ApiResponse from "./middlewares/easyResponse";
+import { morganMiddleware } from "./middlewares/morgan.middleware";
 import globalErrorHandler from "./middlewares/errorHandler";
+import ApiResponse from "./middlewares/easyResponse";
 import lazyRouter from "./utils/lazyLoadRoutes";
 import AppError from "./utils/AppError";
-import { morganMiddleware } from "./middlewares/morgan.middleware";
 
 const app = express();
 
 // Middleware
+app.use(xss());
 app.use(cors());
+app.use(helmet());
+app.use(CookieParser());
+app.use(mongoSanitize());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(CookieParser());
 app.use(morganMiddleware);
 
 app.use(ApiResponse.easyResponse());
