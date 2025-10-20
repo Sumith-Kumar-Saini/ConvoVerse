582915e7e158e35af9da0415f464e71273fd84f0
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-10-20 18:00:48 +0530

    refactor: removed unnecessary properties

--- server/src/models/chat.model.ts
+++ server/src/models/chat.model.ts
@@ -12,21 +12,9 @@ const ChatSchema = new Schema<IChat>(
 
     // Basic info
     title: { type: String, default: "Untitled" }, // user-editable title
-    autoTitle: { type: String }, // AI-generated quick title
     status: { type: String, enum: ["active", "archived"], default: "active" },
 
-    // Linking with messages
-    lastMessageAt: { type: Date },
-
-    // model: {
-    //   provider: { type: String, default: "google" },
-    //   modelId: { type: String, default: "google/gemini-1.5-pro" },
-    //   temperature: { type: Number, default: 0.7 },
-    //   maxTokens: { type: Number, default: 2048 },
-    // },
-
     // Quick metadata
-    tags: [{ type: String }],
     pinned: { type: Boolean, default: false },
   },
   {
