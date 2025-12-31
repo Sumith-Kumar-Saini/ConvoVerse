d2f032bf74b7c8193bed67bb0ff33898c608ce1c
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-12-31 23:29:24 +0530

    comment(sockets): add msg:join/send handlers and simplify message ack

--- apps/server/src/sockets/handlers/chat.handler.ts
+++ apps/server/src/sockets/handlers/chat.handler.ts
@@ -1,16 +1,13 @@
-import { Socket } from "socket.io";
+import { Socket } from 'socket.io';
 
-import { ISocketEventHandler } from "../interfaces/ISocketEventHandler";
+import { ISocketEventHandler } from '../interfaces/ISocketEventHandler';
 
 export class ChatHandler implements ISocketEventHandler {
   register(socket: Socket): void {
-    socket.on(
-      "chat:open",
-      ({ chatId }: { chatId: string }, ack?: (response: any) => void) => {
-        // load the STM to memory
-        // give ack to client socket for next process
-        // ( optional ) ping the worker running on BullMQ
-      }
-    );
+    socket.on('chat:open', ({ chatId }: { chatId: string }, ack?: (response: any) => void) => {
+      // load the STM to memory
+      // give ack to client socket for next process
+      // ( optional ) ping the worker running on BullMQ
+    });
   }
 }
--- apps/server/src/sockets/handlers/message.handler.ts
+++ apps/server/src/sockets/handlers/message.handler.ts
@@ -1,15 +1,29 @@
-import { Socket } from "socket.io";
+import { Socket } from 'socket.io';
 
-import { ISocketEventHandler } from "../interfaces/ISocketEventHandler";
+import { ISocketEventHandler } from '../interfaces/ISocketEventHandler';
 
 export class MessageHandler implements ISocketEventHandler {
   register(socket: Socket): void {
-    socket.on("message", (data: any, ack?: (response: any) => void) => {
-      ack && ack(this.generateResponse());
+    socket.on('message', (data: any, ack?: (response: any) => void) => {
+      ack && ack('hello world');
     });
-  }
 
-  private generateResponse(): string {
-    return Math.random().toString(36).slice(2, 12);
+    socket.on('msg:join', ({ chatId }, ask?: (response: unknown) => void) => {
+      // user can join to stream if we want
+    });
+
+    socket.on(
+      'msg:send',
+      (
+        { chatId, message }: { chatId: string; message: string },
+        ask?: (response: unknown) => void,
+      ) => {
+        // check if the chat id exits or not
+        // send the job to llm worker
+        // after that send the job id and join the user with chat id
+        // stream the content via chat id room with redis pub/sub
+        // end the stream using redis pub/sub
+      },
+    );
   }
 }
